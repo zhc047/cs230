2646c695f4b4f610efd6d531f67a4a23e64fc202
Ubuntu <ubuntu@ip-172-31-17-32.us-west-2.compute.internal>
2019-05-13 01:24:43 +0000

    convert image issue debugging

--- img2StyleganV2.py
+++ img2StyleganV2.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import stylegan.dnnlib as dnnlib
 import sys
 sys.modules["dnnlib"] = dnnlib
+from dnnlib.tflib.tfutil import convert_images_to_uint8
 import stylegan.dnnlib.tflib as tflib
 import stylegan.config as config
 import pickle
@@ -17,7 +18,7 @@ slim = tf.contrib.slim
 num_styles = 18
 dlatent_size = 512
 lamda = 1
-num_epochs = 1000
+num_epochs = 100
 
 # Initialize TensorFlow.
 tflib.init_tf()
@@ -30,52 +31,58 @@ with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
 # Print network details.
 Gs.print_layers()
 
-W = initial_W = tf.get_variable('W', initializer=Gs.get_var('dlatent_avg'))
-W = tf.reshape(tf.tile(W, [num_styles]), [1, dlatent_size, num_styles])
-W = tf.transpose(W, perm=[0, 2, 1])
+W_np = Gs.get_var('dlatent_avg')
+W_np = np.reshape(np.tile(W_np, [num_styles]), [1, dlatent_size, num_styles])
+W_np = np.transpose(W_np, [0, 2, 1])
+W = tf.get_variable('W', initializer=W_np)
 
 generated_img = Gs.components.synthesis.get_output_for(W)
-generated_img = tf.transpose(generated_img, perm=[0, 3, 2, 1])
+# generated_img = tf.transpose(generated_img, perm=[0, 3, 2, 1])
+generated_img = big_generated_img = convert_images_to_uint8(generated_img, nchw_to_nhwc=True)
 generated_img = tf.image.resize_images(generated_img, size=[224,224])
 generated_img = preprocess_input(generated_img)
 # generated_img = slim.preprocessing.vgg_preprocessing.preprocess_image(generated_img)
 
-real_img = load_img('example.png', target_size=[224,224])
+real_img = load_img('example.png')
 real_img = tf.constant(real_img)
-real_img = tf.expand_dims(real_img, 0)
-real_img = tf.cast(real_img, tf.float32)
+real_img = big_real_img = tf.expand_dims(real_img, 0)
+# big_real_img = tf.cast(big_real_img, tf.float32)
+real_img = tf.image.resize_images(real_img, size=[224, 224])
 real_img = preprocess_input(real_img)
 
-_, end_points_real = vgg.vgg_16(real_img)
-_, end_points_generated = vgg.vgg_16(generated_img)
+# _, end_points_real = vgg.vgg_16(real_img)
+# _, end_points_generated = vgg.vgg_16(generated_img)
+#
+# layers = ['vgg_16/conv1/conv1_1', 'vgg_16/conv1/conv1_2', 'vgg_16/conv3/conv3_2', 'vgg_16/conv4/conv4_2']
 
-layers = ['vgg_16/conv1/conv1_1', 'vgg_16/conv1/conv1_2', 'vgg_16/conv3/conv3_2', 'vgg_16/conv4/conv4_2']
+loss = pix_loss = lamda * tf.norm(big_real_img - big_generated_img)
+# for layer in layers:
+#     activation_generated = end_points_generated[layer]
+#     activation_real = end_points_real[layer]
+#     loss += tf.norm(activation_generated - activation_real)
 
-loss = lamda * tf.norm(real_img - generated_img)
-for layer in layers:
-    activation_generated = end_points_generated[layer]
-    activation_real = end_points_real[layer]
-    loss += tf.norm(activation_generated - activation_real)
-
-optim = tf.train.AdamOptimizer()
-train_op = optim.minimize(loss, var_list=[initial_W])
+optim = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optim.minimize(loss, var_list=[W])
 
 with tf.Session() as sess:
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-    restore = slim.assign_from_checkpoint_fn(
-               'vgg_16.ckpt',
-               slim.get_model_variables("vgg_16")
-              )
+    # restore = slim.assign_from_checkpoint_fn(
+    #            'vgg_16.ckpt',
+    #            slim.get_model_variables("vgg_16")
+    #           )
     sess.run(init_op)
-    restore(sess)
+    # restore(sess)
     for i in range(num_epochs):
-        _, loss_val = sess.run([train_op, loss])
+        _, loss_val, pix_loss_val = sess.run([train_op, loss, pix_loss])
         print("epoch " + str(i) + ": " + str(loss_val) + "\n")
+        # print(pix_loss_val)
 
     optimal_img = Gs.components.synthesis.get_output_for(W)
+    optimal_img = convert_images_to_uint8(optimal_img, nchw_to_nhwc=True)
     optimal_img = sess.run(optimal_img)
-    optimal_img = np.transpose(optimal_img, [0, 3, 2, 1])
-    print(optimal_img.shape)
+    print(np.max(optimal_img), np.min(optimal_img))
+    # optimal_img = np.transpose(optimal_img, [0, 3, 2, 1])
+
     os.makedirs(config.result_dir, exist_ok=True)
     png_filename = os.path.join(config.result_dir, 'optimal.png')
     PIL.Image.fromarray(optimal_img[0], 'RGB').save(png_filename)
