d1854a64054b74bd2818db44ad29559465468d0b
Ubuntu <ubuntu@ip-172-31-17-32.us-west-2.compute.internal>
2019-05-18 02:19:23 +0000

    milestone

--- img2StyleganV2.py
+++ img2StyleganV2.py
@@ -22,13 +22,14 @@ def write_summary(value, tag, global_step, summary_writer):
     summary = tf.Summary()
     summary.value.add(tag=tag, simple_value=value)
     summary_writer.add_summary(summary, global_step)
-summary_epoch = 1
 
+summary_epoch = 500
 num_styles = 18
 dlatent_size = 512
 lamda = 1
-num_epochs = 0
-learning_rate = 1e-6
+num_epochs = 5000
+learning_rate = 1e-2
+intp_lamda = 0.5
 
 # Initialize TensorFlow.
 tflib.init_tf()
@@ -38,68 +39,67 @@ url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karra
 with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
     _G, _D, Gs = pickle.load(f)
 
-# Print network details.
-Gs.print_layers()
-
+# get W
 W_np = Gs.get_var('dlatent_avg')
-W_np = np.reshape(np.tile(W_np, [num_styles]), [1, dlatent_size, num_styles])
-W_np = np.transpose(W_np, [0, 2, 1])
+W_np = np.reshape(np.tile(W_np, [2, num_styles]), [2, num_styles, dlatent_size])
 W = tf.get_variable('optimal_dlatent', initializer=W_np)
 tf.summary.histogram('optimal_dlatent', W)
-# Z = np.random.randn(1, 512)
-# W = Gs.components.mapping.get_output_for(tf.get_variable('Z', initializer=Z), None)
-# Apply truncation trick.
-# with tf.variable_scope('Truncation'):
-#     layer_idx = np.arange(num_styles)[np.newaxis, :, np.newaxis]
-#     ones = np.ones(layer_idx.shape, dtype=np.float32)
-#     coefs = tf.where(layer_idx < 8, 0.7 * ones, ones)
-#     W = tflib.lerp(Gs.get_var('dlatent_avg'), W, coefs)
-# tf.summary.histogram('W', W)
-generated_img = Gs.components.synthesis.get_output_for(W, randomize_noise=False, structure='linear', is_validation=True)
+
+generated_img = Gs.components.synthesis.get_output_for(W, randomize_noise=False, structure='linear')
+tf.summary.image('images/generated', convert_images_to_uint8(generated_img, nchw_to_nhwc=True))
 generated_img = tf.transpose(generated_img, perm=[0, 2, 3, 1])
 tf.summary.histogram('float_imgs/generated', generated_img)
 
-real_img = load_img('example.png')
-real_img = np.expand_dims(real_img, 0)
+W_synthesis = intp_lamda * W[0] + (1 - intp_lamda) * W[1]
+W_synthesis = tf.expand_dims(W_synthesis, 0)
+img_synthesis = Gs.components.synthesis.get_output_for(W_synthesis)
+img_synthesis = convert_images_to_uint8(img_synthesis, nchw_to_nhwc=True)
+tf.summary.image('images/synthesis', img_synthesis)
+
+real_img1 = load_img('andrew.png')
+real_img2 = load_img('example.png')
+real_img1 = np.expand_dims(real_img1, 0)
+real_img2 = np.expand_dims(real_img2, 0)
+real_img = np.vstack([real_img1, real_img2])
+tf.summary.image('images/real', real_img)
 real_img = preprocess_input(real_img, mode='tf')
 tf.summary.histogram('float_imgs/real', real_img)
-real_img = (real_img + 1) * (255 / 2)
-tf.summary.histogram('int_imgs/real', real_img)
-generated_img =  (generated_img + 1) * (255 / 2)
-tf.summary.histogram('int_imgs/genereated', generated_img)
-loss = lamda * tf.norm(real_img - generated_img)
+big_real_img = (real_img + 1) * (255 / 2)
+tf.summary.histogram('int_imgs/real', big_real_img)
+big_generated_img =  (generated_img + 1) * (255 / 2)
+tf.summary.histogram('int_imgs/genereated', big_generated_img)
+loss = lamda * tf.norm(big_real_img - big_generated_img)
 
-# generated_img = tf.image.resize_images(generated_img, size=[224,224])
-# real_img = tf.image.resize_images(real_img, size=[224, 224])
-# _, end_points_real = vgg.vgg_16(real_img)
-# _, end_points_generated = vgg.vgg_16(generated_img)
+generated_img = tf.image.resize_images(generated_img, size=[224,224])
+real_img = tf.image.resize_images(real_img, size=[224, 224])
+_, end_points_generated = vgg.vgg_16(generated_img)
+_, end_points_real = vgg.vgg_16(real_img)
 
-# loss = 0
-# layers = ['vgg_16/conv1/conv1_1', 'vgg_16/conv1/conv1_2', 'vgg_16/conv3/conv3_2', 'vgg_16/conv4/conv4_2']
-# for layer in layers:
-#     activation_generated = end_points_generated[layer]
-#     activation_real = end_points_real[layer]
-#     loss += tf.norm(activation_generated - activation_real)
+layers = ['vgg_16/conv1/conv1_1', 'vgg_16/conv1/conv1_2', 'vgg_16/conv3/conv3_2', 'vgg_16/conv4/conv4_2']
+for layer in layers:
+    activation_generated = end_points_generated[layer]
+    activation_real = end_points_real[layer]
+    loss += tf.norm(activation_generated - activation_real)
 
-# optim = tf.train.AdamOptimizer(learning_rate=0.01)
-# train_op = optim.minimize(loss, var_list=[W])
+optim = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = tf.contrib.layers.optimize_loss(
-    loss, tf.train.get_global_step(), learning_rate, 'SGD', summaries=['gradients'], variables=[W]
+    loss, tf.train.get_global_step(), None, optim, summaries=['gradients'], variables=[W]
 )
 
 merge_summaries = tf.summary.merge_all()
 
-with tf.Session() as sess:
+with tf.get_default_session() as sess:
     # Create summary writter for tensorboard
     summary_writer = tf.summary.FileWriter(experiment_dir, sess.graph)
 
-    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-    # restore = slim.assign_from_checkpoint_fn(
-    #            'vgg_16.ckpt',
-    #            slim.get_model_variables("vgg_16")
-    #           )
-    sess.run(init_op)
-    # restore(sess)
+    restore = slim.assign_from_checkpoint_fn(
+               'vgg_16.ckpt',
+               slim.get_model_variables("vgg_16")
+              )
+    restore(sess)
+    sess.run(W.initializer)
+    sess.run(tf.variables_initializer(optim.variables()))
+
     _, summeries = sess.run([loss, merge_summaries])
     summary_writer.add_summary(summeries, 0)
     i = 0
@@ -115,15 +115,23 @@ with tf.Session() as sess:
         if i % summary_epoch == 0:
             summary_writer.add_summary(outputs[2], i+1)
 
-    # optimal_img = Gs.components.synthesis.get_output_for(W, use_noise=False, randomize_noise=False, blur_filter=None)
-    optimal_img = Gs.components.synthesis.get_output_for(W, use_noise=False)
+    optimal_img = Gs.components.synthesis.get_output_for(W)
     tf.summary.histogram('output_img/float', optimal_img)
     optimal_img = convert_images_to_uint8(optimal_img, nchw_to_nhwc=True)
     tf.summary.histogram('output_img/int', optimal_img)
     optimal_img, summaries = sess.run([optimal_img, merge_summaries])
-    print(np.max(optimal_img), np.min(optimal_img))
     summary_writer.add_summary(summaries, i)
 
     os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'optimal.png')
-    PIL.Image.fromarray(optimal_img[0], 'RGB').save(png_filename)
+    for i, img in enumerate(optimal_img):
+        png_filename = os.path.join(config.result_dir, 'optimal{}.png'.format(i))
+        PIL.Image.fromarray(optimal_img[0], 'RGB').save(png_filename)
+
+    W_synthesis = intp_lamda * W[0] + (1 - intp_lamda) * W[1]
+    W_synthesis = tf.expand_dims(W_synthesis, 0)
+    img_synthesis = Gs.components.synthesis.get_output_for(W_synthesis)
+    img_synthesis = convert_images_to_uint8(img_synthesis, nchw_to_nhwc=True)
+    img_synthesis = sess.run([img_synthesis])
+
+    png_filename = os.path.join(config.result_dir, 'synthesis.png')
+    PIL.Image.fromarray(img_synthesis[0], 'RGB').save(png_filename)
