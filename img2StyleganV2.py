000717f5f80bba5e9a8739ce6cac0395c8b7536a
Ubuntu <ubuntu@ip-172-31-17-32.us-west-2.compute.internal>
2019-06-04 07:40:17 +0000

    adjusting loss function, num styles and style selection

--- img2StyleganV2.py
+++ img2StyleganV2.py
@@ -24,12 +24,22 @@ def write_summary(value, tag, global_step, summary_writer):
     summary_writer.add_summary(summary, global_step)
 
 summary_epoch = 500
-num_styles = 18
+num_styles = 6
 dlatent_size = 512
 lamda = 1
-num_epochs = 5000
+num_epochs = 0
 learning_rate = 1e-2
 intp_lamda = 0.5
+syn_loss_lamda = 100
+
+#----------------------------------------------------------------------------
+# Convenience func that casts all of its arguments to tf.float32.
+
+def fp32(*values):
+    if len(values) == 1 and isinstance(values[0], tuple):
+        values = values[0]
+    values = tuple(tf.cast(v, tf.float32) for v in values)
+    return values if len(values) >= 2 else values[0]
 
 # Initialize TensorFlow.
 tflib.init_tf()
@@ -37,13 +47,14 @@ tflib.init_tf()
 # Load pre-trained network.
 url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
 with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
-    _G, _D, Gs = pickle.load(f)
+    _G, D, Gs = pickle.load(f)
 
 # get W
 W_np = Gs.get_var('dlatent_avg')
 W_np = np.reshape(np.tile(W_np, [2, num_styles]), [2, num_styles, dlatent_size])
 W = tf.get_variable('optimal_dlatent', initializer=W_np)
 tf.summary.histogram('optimal_dlatent', W)
+# W_np = np.reshape(np.tile(W_np, [3]), [2, num_styles * 3, dlatent_size])
 
 generated_img = Gs.components.synthesis.get_output_for(W, randomize_noise=False, structure='linear')
 tf.summary.image('images/generated', convert_images_to_uint8(generated_img, nchw_to_nhwc=True))
@@ -51,8 +62,12 @@ generated_img = tf.transpose(generated_img, perm=[0, 2, 3, 1])
 tf.summary.histogram('float_imgs/generated', generated_img)
 
 W_synthesis = intp_lamda * W[0] + (1 - intp_lamda) * W[1]
+
+# W_synthesis[:255] = W[0][:255]
 W_synthesis = tf.expand_dims(W_synthesis, 0)
+
 img_synthesis = Gs.components.synthesis.get_output_for(W_synthesis)
+# fake_scores_out = fp32(D.get_output_for(img_synthesis, None))
 img_synthesis = convert_images_to_uint8(img_synthesis, nchw_to_nhwc=True)
 tf.summary.image('images/synthesis', img_synthesis)
 
@@ -69,6 +84,9 @@ tf.summary.histogram('int_imgs/real', big_real_img)
 big_generated_img =  (generated_img + 1) * (255 / 2)
 tf.summary.histogram('int_imgs/genereated', big_generated_img)
 loss = lamda * tf.norm(big_real_img - big_generated_img)
+
+# synthesis loss
+# loss += syn_loss_lamda * (1 - fake_scores_out)
 
 generated_img = tf.image.resize_images(generated_img, size=[224,224])
 real_img = tf.image.resize_images(real_img, size=[224, 224])
@@ -115,7 +133,7 @@ with tf.get_default_session() as sess:
         if i % summary_epoch == 0:
             summary_writer.add_summary(outputs[2], i+1)
 
-    optimal_img = Gs.components.synthesis.get_output_for(W)
+    # optimal_img = Gs.components.synthesis.get_output_for(W)
     tf.summary.histogram('output_img/float', optimal_img)
     optimal_img = convert_images_to_uint8(optimal_img, nchw_to_nhwc=True)
     tf.summary.histogram('output_img/int', optimal_img)
@@ -129,7 +147,7 @@ with tf.get_default_session() as sess:
 
     W_synthesis = intp_lamda * W[0] + (1 - intp_lamda) * W[1]
     W_synthesis = tf.expand_dims(W_synthesis, 0)
-    img_synthesis = Gs.components.synthesis.get_output_for(W_synthesis)
+    # img_synthesis = Gs.components.synthesis.get_output_for(W_synthesis)
     img_synthesis = convert_images_to_uint8(img_synthesis, nchw_to_nhwc=True)
     img_synthesis = sess.run([img_synthesis])
 
